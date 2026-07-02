1a9986d29f5e1a7b70236597f9514739c2307453
s23bdocs1m01081-wq <s23bdocs1m01081@iub.edu.pk>
2026-07-02 20:14:12 +0500

    vercel.json

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-let 
-cors = require('cors');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
 
 dotenv.config();
 
- cors = require('cors');
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173',
   credentials: true // if you use cookies/auth
 }));
 
@@ -29,19 +27,22 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Bus Booking API");
 });
 
-// ✅ MongoDB connection
-mongoose.connect(process.env.MONGO_URI , {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log("✅ MongoDB connected"))
 .catch((err) => console.error('MongoDB connection error:', err));
-// ✅ Start server
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`✅ Server is running on port ${process.env.PORT || 5000}`);
-});
 
 // ✅ Export app (for Vercel)
 module.exports = app;
+
+if (require.main === module) {
+  const port = process.env.PORT || 5000;
+
+  app.listen(port, () => {
+    console.log(`✅ Server is running on port ${port}`);
+  });
+}
 
 
